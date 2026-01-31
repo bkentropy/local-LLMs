4bb19d77e0ec8976a700ae22fa9e0136040bd72a
Brian Kustra <bkustrakb@gmail.com>
2026-01-31 13:11:34 -0700

    use streaming

--- chat_with_llama.py
+++ chat_with_llama.py
@@ -8,9 +8,9 @@ Answer: Let's think step by step."""
 prompt = ChatPromptTemplate.from_template(template)
 
 model = OllamaLLM(model="llama3.1:8b")
-# model = OllamaLLM(model="qwen2.5-coder:1.5b-base")
 
 chain = prompt | model
 
-response = chain.invoke({"question": "What is LangChain?"})
-print(response)
+for chunk in chain.stream({"question": "What is LangChain?"}):
+    print(chunk, end="", flush=True)
+print()
